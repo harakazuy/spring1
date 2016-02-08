3a3ca70369538565037a6ec7a2da1871d8d91109
harakazuy <gug919@yahoo.co.jp>
2016-02-08 22:08:43 +0900

    first commit

---
+++ MySampleApp1/src/main/java/jp/hara/spring/sample1/App.java
@@ -0,0 +1,11 @@
+package jp.hara.spring.sample1;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello STS World!");
+	}
+
+}
